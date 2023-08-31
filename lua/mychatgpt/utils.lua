9f569139f3525e2634919c1949f235f5bce7e17c
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-08-31 15:10:16 -0300

    feat(utils): add func to get the current line diagnostics

--- lua/mychatgpt/utils.lua
+++ lua/mychatgpt/utils.lua
@@ -16,7 +16,7 @@ function M.get_selection_lines()
   lines[#lines] = lines[#lines]:sub(1, end_col)
   lines[1] = lines[1]:sub(start_col)
 
-  return lines
+  return lines, start_line, end_line, start_col, end_col
 end
 
 function M.get_buf_filetype()
@@ -36,6 +36,28 @@ end
 function M.add_code_block_for_filetype(lines, filetype)
   table.insert(lines, 1, '```' .. filetype)
   table.insert(lines, '```')
+end
+
+---@return { message: string, severity: integer }[]|nil
+function M.get_line_diagnostics()
+  local line = vim.fn.line('.') - 1
+  local bufnr = vim.api.nvim_get_current_buf()
+  local diagnostics = vim.diagnostic.get(bufnr, { lnum = line, severity = { min = vim.diagnostic.severity.HINT } })
+
+  if #diagnostics == 0 then return nil end
+
+  ---@type table[]
+  local obj = {}
+  for _, diagnostic in ipairs(diagnostics) do
+    table.insert(obj, {
+      ---@type string
+      message = diagnostic.message,
+      ---@type integer
+      severity = vim.diagnostic.severity[diagnostic.severity],
+    })
+  end
+
+  return obj
 end
 
 return M
