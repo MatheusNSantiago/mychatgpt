92131a0d2c3ef2d86bd9f5f3ea5e27caac9f5819
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-09-02 22:02:43 -0300

    chore(picker): remove obsolete code

--- lua/mychatgpt/actions/picker.lua
+++ lua/mychatgpt/actions/picker.lua
@@ -21,52 +21,41 @@ function M.create_picker(opts)
 	end
 
 	vim.keymap.set({ 'n', 'x' }, opts.keymap, function()
-		local delay_ms = 0
 		local selection = nil
 
-		-- Caso estaja em modo visual, o nvim precisa de um pouco de mais tempo
-		-- para processar os marks '< e '>, então é necessário adicionar um delay
 		local isVisualMode = vim.fn.mode() == 'v' or vim.fn.mode() == 'V'
-		if isVisualMode then
-			selection = Selection.get_selection()
-			-- -- Sai do modo visual (para que os registradores '<,'> sejam preenchidos)
-			-- local ESC_FEEDKEY = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
-			-- vim.api.nvim_feedkeys(ESC_FEEDKEY, 'n', true)
-			-- delay_ms = 100
-		end
+		if isVisualMode then selection = Selection.get_selection() end
 
-		vim.defer_fn(function()
-			require('telescope.pickers')
-					.new(require('telescope.themes').get_dropdown({}), {
-						prompt_title = opts.title,
-						finder = require('telescope.finders').new_table({
-							results = opts.options,
-							entry_maker = entry_maker,
-						}),
-						sorter = require('telescope.sorters').get_generic_fuzzy_sorter(),
-						attach_mappings = function(prompt_buffer_number)
-							local telescope_action = require('telescope.actions')
+		require('telescope.pickers')
+				.new(require('telescope.themes').get_dropdown({}), {
+					prompt_title = opts.title,
+					finder = require('telescope.finders').new_table({
+						results = opts.options,
+						entry_maker = entry_maker,
+					}),
+					sorter = require('telescope.sorters').get_generic_fuzzy_sorter(),
+					attach_mappings = function(prompt_buffer_number)
+						local telescope_action = require('telescope.actions')
 
-							-- On item select
-							telescope_action.select_default:replace(function()
-								local state = require('telescope.actions.state')
-								local selected_entry = state.get_selected_entry()
+						-- On item select
+						telescope_action.select_default:replace(function()
+							local state = require('telescope.actions.state')
+							local selected_entry = state.get_selected_entry()
 
-								telescope_action.close(prompt_buffer_number) -- Closing the picker
+							telescope_action.close(prompt_buffer_number) -- Closing the picker
 
-								if selection then
-									actions.execute_action(selected_entry.value, function(prompt)
-										opts.callback(prompt, selection) -- executa o callback
-									end)
-								else
-									-- opts.callback(prompt, selection) -- executa o callback
-								end
-							end)
-							return true
-						end,
-					})
-					:find()
-		end, delay_ms)
+							if selection then
+								actions.execute_action(selected_entry.value, function(prompt)
+									opts.callback(prompt, selection) -- executa o callback
+								end)
+							else
+								-- opts.callback(prompt, selection) -- executa o callback
+							end
+						end)
+						return true
+					end,
+				})
+				:find()
 	end)
 end
 
