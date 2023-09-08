b54485e76132033fafd4ce1745510ef5aab23f91
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-09-08 18:50:58 -0300

    fix(message): make is_hidden into a class variable

--- lua/mychatgpt/message.lua
+++ lua/mychatgpt/message.lua
@@ -3,26 +3,26 @@ local utils = require('mychatgpt.utils')
 
 local Message = classes.class()
 
----@class MessageOptions
+---@class Message
 ---@field role string 'user' | 'system' | 'assistant'
 ---@field lines string[]
----@field start_line integer O linha onde começa a mensagem em relação a source
+---@field start_line? integer (default 0) O linha onde começa a mensagem em relação a source
 ---@field is_hidden? boolean (default false) Se a mensagem deve ser renderizada ou não
 
---- @param args MessageOptions
+--- @param args Message
 function Message:init(args)
   local lines = args.lines
-  local is_hidden = args.is_hidden == nil and false or args.is_hidden
+  self.is_hidden = args.is_hidden == nil and false or args.is_hidden
 
   self.role = args.role
 
   table.insert(lines, '') -- adiciona uma linha no final (margin)
 
   self.lines = lines
-  self.start_line = args.start_line
+  self.start_line = args.start_line or 0
   self.end_line = self.start_line + #lines - 1
 
-  if is_hidden then
+  if self.is_hidden then
     self.start_line = 0
     self.end_line = 0
   end
