d7a96e4234d3cfee9ca21d54eaa067b956947729
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-12-02 13:19:24 -0300

    feat(utils): add augroup api wrapper

--- lua/mychatgpt/utils.lua
+++ lua/mychatgpt/utils.lua
@@ -52,4 +52,50 @@ function M.concat_lists(...)
 end
 
 
+---@class AutocmdArgs
+---@field id number autocmd ID
+---@field event string
+---@field group string?
+---@field buf number
+---@field file string
+---@field match string | number
+---@field data any
+
+---@class Autocommand
+---@field desc string?
+---@field event  string | string[] list of autocommand events
+---@field pattern string | string[] | nil list of autocommand patterns
+---@field command string | fun(args: AutocmdArgs): boolean?
+---@field nested  boolean?
+---@field once    boolean?
+---@field buffer  number?
+
+---Create an autocommand
+---returns the group ID so that it can be cleared or manipulated.
+---@param name string The name of the autocommand group
+---@param ... Autocommand A list of autocommands to create
+---@return number
+function M.augroup(name, ...)
+  local commands = { ... }
+  assert(name ~= 'User', 'The name of an augroup CANNOT be User')
+  assert(#commands > 0, string.format('You must specify at least one autocommand for %s', name))
+
+  local id = vim.api.nvim_create_augroup(name, { clear = true })
+  for _, autocmd in ipairs(commands) do
+    local is_callback = type(autocmd.command) == 'function'
+
+    vim.api.nvim_create_autocmd(autocmd.event, {
+      group = name,
+      pattern = autocmd.pattern,
+      desc = autocmd.desc,
+      callback = is_callback and autocmd.command or nil,
+      command = not is_callback and autocmd.command or nil,
+      once = autocmd.once,
+      nested = autocmd.nested,
+      buffer = autocmd.buffer,
+    })
+  end
+  return id
+end
+
 return M
